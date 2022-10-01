acc23d2a571337e4d86132a810284a69aa0c0a33
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-10-01 15:05:35 +0000

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+#input para pedir información por teclado
+#Int para capturar información en formato int
+n1= int(input("Dime un número entero: "))
+#Type() para capturar el tipo de dato
+print(n1,(type(n1)))
+#################################
+n2= int(input("Dime un número flotante: "))
+print(n2, type(n2))
