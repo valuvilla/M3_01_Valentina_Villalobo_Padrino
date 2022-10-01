0c4549b4f1abf5bc74e2e3c508b10146bbe487c8
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-10-01 15:32:47 +0000

    corrección ejercicio2

--- ejercicio2.py
+++ ejercicio2.py
@@ -5,7 +5,7 @@ e1= int(input("Dime un número entero: "))
 print("{:05d}".format(e1))
 ###########################
 #Creamos otro variable
-e2=float(input("Dime un número flotante"))
+e2=float(input("Dime un número flotante: "))
 #Presentamos el número con 5 digita en la parte entera y 3 en la decimal.
 print("{:09.03f}".format(e2))
 
